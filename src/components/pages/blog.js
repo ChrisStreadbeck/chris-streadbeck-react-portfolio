9262813ca79c95b959a1aa718bfb8d175ee31cde
ChrisStreadbeck <han0987@gmail.com>
2019-07-23 14:54:53 -0600

    started work on blog feature

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+import BlogItem from "../blog/blog-item";
 
-      <div>
-        <Link to="/about-me">Read more about myself</Link>
-      </div>
-    </div>
-  );
+class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://chrisstreadbeck.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
+    return <div>{blogRecords}</div>;
+  }
 }
+
+export default Blog;
