32566c26c7b9642a24520474c7bd1864885532ea
ChrisStreadbeck <han0987@gmail.com>
2019-07-29 13:38:15 -0600

    added resume link to about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -46,6 +46,13 @@ export default function() {
           >
             My GitHub
           </a>
+          <a
+            className="github"
+            href="https://drive.google.com/file/d/1qE-YRyMXHUdM_zWvX2yzcpx89oLzq2kY/view?ths=true"
+            target="_blank"
+          >
+            My Resume
+          </a>
         </div>
       </div>
     </div>
